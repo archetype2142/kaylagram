1b7a12f415273646180d2d477102d37f607f8732
Ritwick <malhotraritwick2011@gmail.com>
2016-05-07 17:04:33 +0530

    KaylaGram v 0.0.1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def new
+		@post = Post.new
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create 
+		@post =  Post.create(post_params)
+		if @post.save
+			flash[:success] = "Success!"
+			redirect_to post_path(@post)
+
+		else
+			flash[:error] = @post.errors.full_messages
+			redirect_to new_path
+		end
+	end 
+	private
+
+	def post_params
+		params.require(:post).permit!
+	end
+end
