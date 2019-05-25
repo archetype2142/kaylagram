1b1ae9120730983caed616728d0fc697009d9e4a
archetype2142 <malhotraritwick2011@gmail.com>
2019-05-25 20:54:38 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,9 +101,9 @@ Rails.application.configure do
 end
 
 AssetSync.configure do |config|
-  config.fog_provider = ‘AWS’
-  config.aws_access_key_id = ENV.fetch(‘AWS_ACCESS_KEY_ID_IAM’)
-  config.aws_secret_access_key = ENV.fetch(‘AWS_SECRET_ACCESS_KEY_IAM’)
-  config.fog_directory = ENV.fetch(‘ASSET_S3_BUCKET’)
-  config.fog_region = ENV.fetch(‘AWS_S3_REGION’)
+  config.fog_provider = 'AWS'
+  config.aws_access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID_IAM')
+  config.aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY_IAM')
+  config.fog_directory = ENV.fetch('ASSET_S3_BUCKE')
+  config.fog_region = ENV.fetch('AWS_S3_REGION')
 end
