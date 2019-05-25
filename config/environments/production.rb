43a5b079e792cb5dd45ddc6ddb351ffcb60574bd
archetype2142 <malhotraritwick2011@gmail.com>
2019-05-25 21:17:09 +0200

    sad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,10 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = true
 end
 
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID_IAM')
-  config.aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY_IAM')
-  config.fog_directory = ENV.fetch('ASSET_S3_BUCKET')
-  config.fog_region = ENV.fetch('AWS_S3_REGION')
-end
+# AssetSync.configure do |config|
+#   config.fog_provider = 'AWS'
+#   config.aws_access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID_IAM')
+#   config.aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY_IAM')
+#   config.fog_directory = ENV.fetch('ASSET_S3_BUCKET')
+#   config.fog_region = ENV.fetch('AWS_S3_REGION')
+# end
